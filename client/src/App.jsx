dfab7758f37e4ef821986a96253cfd55f0632609
iamarobot2 <avinjoshy21@gmail.com>
2024-02-26 12:00:41 +0530

    Add components , setup react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   )
 }
 
---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <main className="container mx-2 my-2 self-center">
+        Welcome
+    </main>
+  )
+}
+
+export default Home
