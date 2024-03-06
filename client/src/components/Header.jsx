3fc719cff9064e72a95512bbe99e38a611898cd7
iamarobot2 <avinjoshy21@gmail.com>
2024-03-06 21:52:29 +0530

    Added Hero Section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,41 +1,52 @@
 import { Link } from "react-router-dom"
 import MenuIcon from '@mui/icons-material/Menu';
 import Switcher from './Switcher';
-import { IconButton } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 
 
 function Header() {
  
   return (
    <>
-   <header className="shadow-lg  mx-auto px-6 py-1 dark:bg-slate-900 dark:text-white flex flex-row justify-between items-center">
-   <img src="/assets/cardiology.png" alt="Logo" className=" aspect-square w-12" />
-    <nav>
-      <ul className="hidden md:flex flex-row gap-6">
-        <li>
-          <Link >Home</Link>
-        </li>
-        <li>
-          <Link >About</Link>
-        </li>
-        <li>
-          <Link >Features</Link>
-        </li>
-        <li>
-          <Link >Resources</Link>
-        </li>
-        <li>
-          <Link>Contact Us</Link>
-        </li>
-      </ul>
-    </nav>
-    <div className="flex flex-row gap-2 md:gap-6 items-center">
+   <header className="shadow-lg shadow-slate-500 w-full  mx-auto px-6 lg:px-12 py-1 dark:bg-slate-900 dark:text-white flex flex-row justify-between items-center">
+   <div className="flex flex-row items-center gap-10">
+    <div className="flex flex-row gap-4 items-center">
+      <img src="/assets/logo3.png" alt="Logo" className=" aspect-square w-12" />
+      <h1 className="text-2xl font-semibold">One Health</h1>
+    </div>
+      <nav>
+        <ul className="hidden lg:flex flex-row gap-6 text-md dark:text-slate-300 text-slate-700">
+          <li className="hover:text-blue-300">
+            <Link >Home</Link>
+          </li>
+          <li className="hover:text-blue-300">
+            <Link >About</Link>
+          </li>
+          <li className="hover:text-blue-300">
+            <Link >Features</Link>
+          </li>
+          <li className="hover:text-blue-300">
+            <Link >Resources</Link>
+          </li>
+          <li className="hover:text-blue-300">
+            <Link>Contact Us</Link>
+          </li>
+        </ul>
+      </nav>
+   </div>
+    <div className="flex flex-row gap-2 lg:gap-6 items-center">
+      <div className="hidden lg:flex flex-row gap-2">
+        <Link to={'/login'}><Button variant="outlined" size="small">Login</Button></Link>
+        <Link to={'/signup'}><Button variant="contained" size="small">Signup</Button></Link>
+      </div>
       <div className="pt-7">
         <Switcher></Switcher>
       </div>
-      <IconButton>
-        <MenuIcon color="primary"/>
-      </IconButton>
+      <div className="lg:hidden">
+        <IconButton aria-label="Menu Button">
+          <MenuIcon color="primary"/>
+        </IconButton>
+      </div>
     </div>
    </header>
    </>
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,8 +1,29 @@
+import { Button } from "@mui/material"
+import { Link } from "react-router-dom"
 
 function Home() {
   return (
-    <section className="dark:bg-slate-900 dark:text-white">
-      {/* <img className=" dark:bg-slate-900" src="/assets/heartbeat.gif" alt="heratbeat.gif" /> */}
+    <section className=" dark:bg-slate-900 w-full bg-[url('/assets/hero1.jpg')] bg-center bg-cover mx-auto px-6 lg:px-12 py-6 lg:py-12 flex flex-col text-xl">
+      <div className="flex flex-col gap-4 lg:w-2/5">
+        <div className="container mx-auto flex gap-4 flex-col items-center backdrop-blur-md bg-white/10 p-6 ">
+          <h2 className="font-semibold">Your Health. Your Control.</h2>
+          <p className="">One Health is revolutionizing healthcare by putting patients at the center. Our Global Medical Record Management System (GMRMS) provides
+             a centralized platform for managing your medical information, empowering you to take control of your health journey.
+          </p>
+        </div>
+        <div className="container mx-auto flex gap-4 flex-col items-center backdrop-blur-md bg-white/10 p-6 ">
+          <h2 className="font-semibold">With One Health, you can:</h2>
+          <ul className="list-disc">
+            <li>Access your complete medical information securely.</li>
+            <li>Book appointments with healthcare providers online.</li>
+            <li>Share your medical data seamlessly with your consent.</li>
+            <li>Manage your insurance information efficiently.</li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex flex-row justify-center py-6">
+        <Link><Button variant="contained" size="large">Signup</Button></Link>
+      </div>
     </section>
   )
 }
